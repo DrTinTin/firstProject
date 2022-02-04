9490b789af3215fa612de4988f513c5c2bd28e8a
Reiner <dingh09@u.nus.edu>
2022-02-04 23:39:56 +0800

    "switch and fortest"

---
+++ src/com/day1/SwitchCaseTest.java
@@ -0,0 +1,17 @@
+package com.day1;
+
+public class SwitchCaseTest {
+  public static void main(String[] args) {
+    int num = 2;
+    switch (num) {
+      case 0:
+        System.out.println("zero");
+        break;
+      case 1:
+        System.out.println("one");
+        break;
+      default:
+        System.out.println("Other");
+    }
+  }
+}
