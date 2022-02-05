2344bfb3ff0feb43ac943bdf25091f17d84726d7
Reiner <dingh09@u.nus.edu>
2022-02-05 10:38:38 +0800

    "scanner"

--- src/com/day1/ScannerTest.java
+++ src/com/day1/ScannerTest.java
@@ -1,5 +1,12 @@
 package com.day1;
 
-public class ScannerTest {
+import java.util.Scanner;
 
+public class ScannerTest {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        System.out.println(num);
+
+    }
 }
