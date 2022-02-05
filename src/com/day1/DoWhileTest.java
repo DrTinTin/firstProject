c33fec4fd1f5e0ea0fcd69f64e1b1da86c1c1dbd
Reiner <dingh09@u.nus.edu>
2022-02-05 10:38:07 +0800

    "do while"

--- src/com/day1/DoWhileTest.java
+++ src/com/day1/DoWhileTest.java
@@ -1,5 +1,14 @@
 package com.day1;
 
 public class DoWhileTest {
-
+  public static void main(String[] args) {
+    //
+    int num = 1;
+    do {
+      if (num % 2 == 0) {
+        System.out.println(num);
+      }
+      num++;
+    } while (num <= 100);
+  }
 }
