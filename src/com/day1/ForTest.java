53b0c0a51520c011a9377d893cc0a76e95cae2ab
Reiner <dingh09@u.nus.edu>
2022-02-05 10:39:04 +0800

    "for"

--- src/com/day1/ForTest.java
+++ src/com/day1/ForTest.java
@@ -1,5 +1,10 @@
 package com.day1;
 
 public class ForTest {
-
+  public static void main(String[] args) {
+    //
+    for (int i = 0; i <= 12; i++) {
+      System.out.println("Hello!");
+    }
+  }
 }
