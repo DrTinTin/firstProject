f18fe2438123096f24c70d9b411ca822bfe1fc22
Elliot <dingh09@u.nus.edu>
2022-02-14 13:18:04 +0800

    BinarySearch

---
+++ Algorithm/src/com/sort/BinarySearch.java
@@ -0,0 +1,32 @@
+package com.sort;
+
+public class BinarySearch {
+  public int binarySearchRec(int[] array, int target, int left, int right) {
+    if (right < left) return -1;
+    int middle = (left + right) / 2;
+    if (array[middle] == target) {
+      return middle;
+    }
+    if (target < array[middle]) {
+      return binarySearchRec(array, target, left, middle - 1);
+    }
+    return binarySearchRec(array, target, middle + 1, right);
+  }
+
+  public int binarySearch(int[] array, int target) {
+    var left = 0;
+    var right = array.length - 1;
+    while (left <= right) {
+      var middle = (left + right) / 2;
+      if (array[middle] == target) {
+        return middle;
+      }
+      if (target < array[middle]) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    }
+    return -1;
+  }
+}
